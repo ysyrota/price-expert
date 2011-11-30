663ab606c1c20e76a3b1f439454e07f01a43e330
Yuriy Syrota <ysyrota@gmail.com>
2011-12-01 00:09:15 +0300

    Implemented POST

--- expert.pl
+++ expert.pl
@@ -1,8 +1,27 @@
 #!/usr/bin/env perl
 
+package Model;
+use ORLite {
+    file => 'prices.db',
+    create => sub {
+        my $dbh = shift;
+        $dbh->do(
+            'CREATE TABLE prices (
+              id INTEGER PRIMARY KEY,
+              seller TEXT NOT NULL,
+              buyer TEXT NOT NULL,
+              article TEXT NOT NULL,
+              date TEXT NOT NULL,
+              price TEXT NOT NULL,
+              comment TEXT NULL)'
+        );
+    }
+};
+
+package main;
+
 use Mojolicious::Lite;
 use Mojo::JSON;
-
 get '/' => sub {
   my $self = shift;
   $self->render('index');
@@ -10,16 +29,33 @@ get '/' => sub {
 
 get '/prices' => sub {
   my $self = shift;
-  $self->respond_to(
-    json => sub { $self->render_json(['prices']) },
-    html => sub { $self->render_data('<html><body>works') },
-    xml  => sub { $self->render_data('<works/>') }
-  );
+  $self->rendered(501);
 };
 
+# insertion
 post '/prices' => sub {
     my $self = shift;
-    $self->rendered(501);
+    my $seller = $self->param('seller');
+    my $buyer = $self->param('buyer');
+    my $article = $self->param('article');
+    my $date = $self->param('date');
+    my $price = $self->param('price');
+    my $comment = $self->param('comment');
+    unless (defined($seller) and defined($buyer) and defined($article) and defined($date) and defined($price)) {
+        $self->render_json({message => 'seller, buyer, article, date and price are obligatory parameters'}, status => 400);
+    } else {
+        my $record = Model::Prices->create(seller => $seller, buyer => $buyer, article => $article, date => $date, price => $price, comment => $comment);
+        $self->render_json(
+            {
+                id  => $record->id,
+                seller => $record->seller,
+                buyer => $record->buyer,
+                article => $record->article,
+                date => $record->date,
+                price => $record->price,
+                comment => $record->comment
+            }, status => 201);
+    }
 };
 
 put '/prices' => sub {
